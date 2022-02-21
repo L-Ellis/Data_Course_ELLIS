8968843d57f085a9d7eb966d0e9b10c4efad9016
L-Ellis <10867944@uvu.edu>
2022-02-20 23:57:52 -0700

    Continue Assignment 6
    I am having some massive issues with the substrate order in the first graph,
    I also have been focused on that for so long that I haven't moved on to more important stuff.

--- Data_Course/Assignments/Assignment_6/Assignment_6.R
+++ Data_Course/Assignments/Assignment_6/Assignment_6.R
@@ -35,26 +35,86 @@ dat %>%
   # nrow()
 
 
-# Faceted Graph #WIP
+# Faceted Graph
+dat <- dat %>% filter(Dilution == 0.1) 
+
+  # Alphabetical order but anything starting with weird characters is put at the end. 
+  # I do this by adding a special character to the beginning of everything starting with a weird character that will bump it to the end of an alphabetical sort
+  # Then, I only use this to order the levels of the actual Substrate data, so the special character is not included in the graph.
+
+non_ASCII_substrate <- dat$Substrate[ grepl(dat$Substrate,pattern="^[^ -~]") ]
+non_ASCII_substrate <- paste0("zzzz", non_ASCII_substrate)
+  # I mean, I could just use zzzzz or something, but this feels cooler. 
+  # https://stackoverflow.com/questions/8086375/what-character-to-use-to-put-an-item-at-the-end-of-an-alphabetic-list
+
+ASCII_substrate <- dat$Substrate[ !grepl(x=dat$Substrate,pattern="^[^ -~]") ]
+substrate_order <- unlist(list(non_ASCII_substrate, ASCII_substrate)) # Combine # https://www.geeksforgeeks.org/concatenate-two-given-factor-in-a-single-factor-in-r/
+
+dat <- dat %>% mutate(Substrate = factor(x=Substrate, levels = sort(substrate_order))) # no NA's here, but it's not sorted right.
+
+dat$Substrate %>% view
+sort(substrate_order) %>% view
+dat$Substrate = factor(dat$Substrate,levels = sort(unique(substrate_order))) %>% view #something wrong with unique??? WHERE ARE THE NA's COMING FROM??
+
 dat %>% 
-  filter(Dilution == 0.1) %>% 
-  
-  # Alphabetical order but anything starting with weird characters are put at the end.
-  group_by(grepl(x=Substrate,pattern="^[^ -~]")) %>%  # group_by anything starting with non-ASCII characters # https://stackoverflow.com/questions/34613761/detect-non-ascii-characters-in-a-string
-  group_modify(
-    mutate(dat$Substrate = factor(dat$Substrate, levels = order(dat$Substrate)))
-    ) %>% 
-  
   ggplot(aes(x=Time,y=Absorbance,color=Sample.Source)) +
   geom_line() +
-  facet_wrap(~ Substrate)
-  
-  
+  facet_wrap(~ Substrate) +
+  theme_bw() 
+
+
+# Animatated Graph
+
+library(gganimate)
+
+
+
+
+
+
+
+
+
+
+
 
   
 
 
 
+
+# ASCII_substrate <- dat$Substrate[ ! grepl(x=unique(dat$Substrate),pattern="^[^ -~]")]
+# ASCII_substrate <- factor(ASCII_substrate,levels=sort(unique(ASCII_substrate)))
+
+# non_ASCII_substrate <- dat$Substrate[grepl(x=dat$Substrate,pattern="^[^ -~]")]
+# non_ASCII_substrate <- factor(non_ASCII_substrate,levels=sort(unique(non_ASCII_substrate) + (length(unique(ASCII_substrate)) - length(unique(non_ASCII_substrate))))) # place at end
+
+# ordered_substrate <- unlist(list(ASCII_substrate,non_ASCII_substrate)) # https://www.geeksforgeeks.org/concatenate-two-given-factor-in-a-single-factor-in-r/
+# levels(ordered_substrate) %>% view
+
+
+#   group_by(grepl(x=Substrate,pattern="^[^ -~]")) %>% 
+#   group_modify(~ mutate(.x, Substrate = factor(Substrate, levels = unique(sort(dat$Substrate))))) %>% 
+
+# sort_ASCII_last <- function(x) {
+#   paste0("ZZZZ", x)
+# }
+
+# ordered_substrate <- dat$Substrate %>% 
+#   apply(MARGIN = 1, FUN=sort_ASCII_last(x=dat$Substrate[grepl(pattern="^[^ -~]")]))
+  
+# group_by(grepl(x=Substrate,pattern="^[^ -~]")) %>% 
+# group_map(~ mutate(Substrate = factor(dat$Substrate, levels = unique(dat$Substrate))), data = .x)
+
+# group by pattern, sort each group alphabetically, merge groups so that levels of group 2 are appended to the end
+
+# group_by anything starting with non-ASCII characters # https://stackoverflow.com/questions/34613761/detect-non-ascii-characters-in-a-string
+
+# mutate(Substrate = factor(x=Substrate,levels=levels(ordered_substrate))) %>% # - levels()
+
+
+any(is.na(substrate_order))
+any(is.na(dat$Substrate))
 
 
 
