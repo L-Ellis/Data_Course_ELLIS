bb26a7bd09ed1df4189eb56bf455606acb4de19f
Leah Ellis <10867944@uvu.edu>
2025-01-28 11:11:47 -0700

    Update Class_Notes_1-28-2025.R

--- Classes/Class_Notes_1-28-2025.R
+++ Classes/Class_Notes_1-28-2025.R
@@ -14,8 +14,36 @@ my_mtcars %>% view
 
 
 ## Option 2: 1 Step
-
 my_mtcars <- mtcars
-my_mtcars <- my_mtcars[my_mtcars$mpg > 20 && my_mtcars$cyl == 4,]
+my_mtcars <- my_mtcars[my_mtcars$mpg > 20 & my_mtcars$cyl == 4,]
 my_mtcars %>% view
 
+## 3. Convert mpg to a character data type.
+my_mtcars <- mtcars
+my_mtcars$mpg <- as.character(my_mtcars$mpg)
+my_mtcars %>% str
+
+## 4. Convert the entire data frame to character data type.
+
+  # Option 1 : do it one at a time. (don't do this one)
+my_mtcars <- mtcars
+my_mtcars$mpg <- as.character(my_mtcars$mpg)
+my_mtcars$cyl <- as.character(my_mtcars$cyl)
+my_mtcars$carb <- as.character(my_mtcars$carb)
+## ...
+my_mtcars %>% str
+
+  # Option 2 : Using a for loop
+my_mtcars <- mtcars
+for (col in names(my_mtcars)) {
+  my_mtcars[, col] <- as.character(my_mtcars[, col])
+}
+my_mtcars %>% str
+
+  # Option 3 : Using apply
+my_mtcars <- mtcars
+my_mtcars <- apply(my_mtcars, 2, as.character)
+my_mtcars %>% as.data.frame %>% str
+
+## 4. Write into a csv file
+write.csv(mtcars, paste0(getwd(),'/Classes/class_practice_1-28-2025.csv'), row.names = F)
