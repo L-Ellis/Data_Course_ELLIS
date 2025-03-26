702c35f85655928cc5605f16363bbd8d07b7d63e
Leah Ellis <10867944@uvu.edu>
2025-03-25 21:32:16 -0600

    continue Assignment 6
    large amount of commits ha- I'm going to and from my friends house because she broke her wrist and I'm switching computers.

--- Assignments/Assignment_6/Assignment_6.R
+++ Assignments/Assignment_6/Assignment_6.R
@@ -1,2 +1,5 @@
 library(tidyverse)
-dat <- read_csv("../../Data/BioLog_Plate_Data.csv") # you may need to modify this path to fit where this file is stored relative to your assignment 6 script
+
+dat <- read_csv("Data/BioLog_Plate_Data.csv") # you may need to modify this path to fit where this file is stored relative to your assignment 6 script
+
+dat %>% pivot_longer(cols = c('Hr_24', 'Hr_48', 'Hr_144'), names_to = "Time (hours)", values_to = c('24', '48', '144')) %>% view
